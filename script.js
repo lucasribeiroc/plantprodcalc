8e622c98db8d25e09d9b830432e33429b1f8c460
Lucas Ribeiro <lucasr.camargo@live.com>
2023-01-20 14:18:12 -0300

    upload - calculadora de rega

---
+++ script.js
@@ -0,0 +1,35 @@
+function calcppmvega() {
+  const ppm = parseFloat(document.querySelector("input").value);
+
+  const resultadopp = (ppm * 46.2) / 100;
+  document.querySelector(".resultadopp").innerHTML =
+    "Plant Prod: " + Math.round(resultadopp) + " ppm";
+
+  const resultadocalnit = (ppm * 32.8) / 100;
+  document.querySelector(".resultadocalnit").innerHTML =
+    "Nitrato de Cálcio: " + Math.round(resultadocalnit) + " ppm";
+
+  const resultadosulfmag = (ppm * 22) / 100;
+  document.querySelector(".resultadosulfmag").innerHTML =
+    "Nitrato de Cálcio: " + Math.round(resultadosulfmag) + " ppm";
+}
+
+function calcppmflora() {
+  const ppm = parseFloat(document.querySelector("input").value);
+
+  const resultadopp = (ppm * 40.7) / 100;
+  document.querySelector(".resultadopp").innerHTML =
+    "Plant Prod: " + Math.round(resultadopp) + " ppm";
+
+  const resultadocalnit = (ppm * 18.5) / 100;
+  document.querySelector(".resultadocalnit").innerHTML =
+    "Nitrato de Cálcio: " + Math.round(resultadocalnit) + " ppm";
+
+  const resultadosulfmag = (ppm * 18.5) / 100;
+  document.querySelector(".resultadosulfmag").innerHTML =
+    "Nitrato de Cálcio: " + Math.round(resultadosulfmag) + " ppm";
+
+  const resultadomkp = (ppm * 23) / 100;
+  document.querySelector(".resultadomkp").innerHTML =
+    "MKP: " + Math.round(resultadomkp) + " ppm";
+}
